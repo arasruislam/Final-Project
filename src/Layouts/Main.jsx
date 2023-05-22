a0293aced39e74e5205c5400c132c9b1e45e9ebb
asru-islam <arasru01@gmail.com>
2023-05-23 02:50:51 +0600

    created a navbar, footer section

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/Header/NavBar";
 
 const Main = () => {
   return (
     <>
+      <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
-import App from "../App";
+import Home from "../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <App />,
+        element: <Home />,
       },
     ],
   },
