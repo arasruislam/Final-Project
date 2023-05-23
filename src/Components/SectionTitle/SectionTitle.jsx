fb47bbc45ee9444ada7e12a362cbb60158ca43c9
asru-islam <arasru01@gmail.com>
2023-05-24 00:07:13 +0600

    add a sectiontitle components

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+
+const SectionTitle = ({Title, SubTitle}) => {
+    return (
+      <div className="mb-8 grid place-items-center">
+        <h4 className=" text-[#D99904] italic border-b-2 pb-2">
+          ---{SubTitle}---
+        </h4>
+        <h1 className="text-4xl border-b-2 py-4">{Title}</h1>
+      </div>
+    );
+};
+
+export default SectionTitle;
